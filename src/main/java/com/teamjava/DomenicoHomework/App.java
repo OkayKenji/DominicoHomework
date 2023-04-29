2db9f154c79748e96c927dccb2e244a961f0e326
Tyler Besnoff <tibesnoff@gmail.com>
2023-04-29 18:58:12 -0400

    First commit

---
+++ src/main/java/com/teamjava/DomenicoHomework/App.java
@@ -0,0 +1,13 @@
+package com.teamjava.DomenicoHomework;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
